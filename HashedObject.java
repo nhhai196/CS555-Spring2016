e9ea684b79f68847aa107eb9cf9ff177f6cac0cf
Duc Viet Le <ledv@mountunion.edu>
2016-04-06 01:12:43 -0400

    Update HashedObject.java

--- HashedObject.java
+++ HashedObject.java
@@ -37,10 +37,10 @@ public class HashedObject {
 	//convert bytes[] into bitstring array
 	private String toBinary(byte[] bytes)
 	{
-	StringBuilder sb = new StringBuilder(bytes.length * Byte.SIZE);
-	for( int i = 0; i < Byte.SIZE * bytes.length; i++ )
-	    sb.append((bytes[i / Byte.SIZE] << i % Byte.SIZE & 0x80) == 0 ? '0' : '1');
-	return sb.toString();
+		StringBuilder sb = new StringBuilder(bytes.length * Byte.SIZE);
+		for( int i = 0; i < Byte.SIZE * bytes.length; i++ )
+		    sb.append((bytes[i / Byte.SIZE] << i % Byte.SIZE & 0x80) == 0 ? '0' : '1');
+		return sb.toString();
 	}
 	
 	//hash with byte key
